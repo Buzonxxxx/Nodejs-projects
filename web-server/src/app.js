abcc3cc8b90e0fed6812b391eff5512d217b3a23
louis.liao <buzonliao@gmail.com>
2019-04-21 23:35:41 +0800

    add html/json in express response

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,20 +3,30 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello express!");
+  res.send("<h1>Weather</h1>");
 });
 
-app.get('/help', (req, res) => {
-  res.send("Help page")
-})
+app.get("/help", (req, res) => {
+  res.send([
+    {
+      name: "Louis"
+    },
+    {
+      name: "Anney"
+    }
+  ]);
+});
 
-app.get('/weather', (req, res) => {
-  res.send("Weather page")
-})
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It's snowing",
+    location: "Taipei"
+  });
+});
 
-app.get('/about', (req, res) => {
-  res.send("About page")
-})
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
+});
 
 app.listen(3000, () => {
   console.log("server is up on port 3000");
