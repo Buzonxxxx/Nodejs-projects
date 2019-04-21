00d65612d342337ef09ae5593c7220c7c79aa60a
louis.liao <buzonliao@gmail.com>
2019-04-21 12:28:30 +0800

    finish weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,13 @@
-const geocode = require("./utils/geocode")
-const request = require("request");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-geocode("Philadelphia", (error, data) => {
-  console.log('Error:', error)
-  console.log('Data:', data)
-})
-// const weatherUrl = `https://api.darksky.net/forecast/e1a90f332437d644e8d4a0b2909526c1/${latitude},${longtitude}`;
-//     request({ url: weatherUrl, json: true }, (error, response) => {
-//       if (error) {
-//         console.log("Unable to connect to weather service!");
-//       } else if (response.body.body) {
-//         console.log("Unable to find location!");
-//       } else {
-//         const { temperature, precipProbability } = response.body.currently;
-//         console.log(
-//           `It is currently ${temperature} degrees out. There is ${precipProbability}% chance of rain.`
-//         );
-//       }
-//     });
+const address = process.argv[2];
+if (!address) return console.log("Please provide an address.");
+geocode(address, (error, { latitude, longtitude, location }) => {
+  if (error) return console.log("Error:", error);
+  forecast(latitude, longtitude, (error, forecastData) => {
+    if (error) return console.log("Error", error);
+    console.log(location);
+    console.log(forecastData);
+  });
+});
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require("request");
+
+const forecast = (latitude, longtitude, callback) => {
+  const url = `https://api.darksky.net/forecast/e1a90f332437d644e8d4a0b2909526c1/${latitude},${longtitude}`;
+  request({ url, json: true }, (error,  response ) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location!", undefined);
+    } else {
+      const { temperature, precipProbability } = response.body.currently;
+      callback(undefined, { temperature, precipProbability });
+    }
+  });
+};
+
+module.exports = forecast;
