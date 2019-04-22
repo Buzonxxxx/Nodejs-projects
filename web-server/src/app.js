76928d1d207b753af7fe74e39234ffe274de796b
Louis Liao <buzonliao@gmail.com>
2019-04-22 16:04:36 +0800

    add html/css/img

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,10 @@
+const path = require('path')
 const express = require("express");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public")
 
-app.get("/", (req, res) => {
-  res.send("<h1>Weather</h1>");
-});
-
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "Louis"
-    },
-    {
-      name: "Anney"
-    }
-  ]);
-});
+app.use(express.static(publicDirectoryPath))
 
 app.get("/weather", (req, res) => {
   res.send({
@@ -24,14 +13,7 @@ app.get("/weather", (req, res) => {
   });
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
-});
-
 app.listen(3000, () => {
   console.log("server is up on port 3000");
 });
 
-// app.com
-// app.com/help
-// app.com/about
