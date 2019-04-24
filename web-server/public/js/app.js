407fdf7148e85e81355bc2ddb921ea20afa86839
Louis Liao <buzonliao@gmail.com>
2019-04-24 18:21:38 +0800

    add fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,7 @@
 console.log("client side javascript is loaded!");
+
+fetch("http://puzzle.mead.io/puzzle").then((response) => {
+  response.json().then((data) => {
+    console.log(data)
+  })
+})
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -88,6 +88,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is up on port 3000");
+app.listen(1234, () => {
+  console.log("server is up on port 1234");
 });
