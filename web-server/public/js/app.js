d66ea1a51d88b688c6023e122363042cc774ce68
Louis Liao <buzonliao@gmail.com>
2019-04-25 14:18:26 +0800

    show weather result in FE

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,16 +2,23 @@ console.log("client side javascript is loaded!");
 
 const weatherForm = document.querySelector("form")
 const searchValue = document.querySelector("input")
+const messageOne = document.querySelector("#location")
+const messageTwo = document.querySelector("#forecast")
 
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const location = searchValue.value;
-  
+  messageOne.textContent = "Loading..."
+  messageTwo.textContent = ""
   fetch(`http://localhost:1234/weather?address=${location}`).then((response) => {
-  response.json().then((data) => {
-    if (data.error) return console.log(data.error)
-    console.log(data.location)
-    console.log(data.forecast)
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+      } 
+      else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
+      }
+    })
   })
-})
 })
