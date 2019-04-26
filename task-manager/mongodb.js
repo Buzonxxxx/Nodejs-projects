1e3583733569499487829e576ec69e14107171f2
louis.liao <buzonliao@gmail.com>
2019-04-26 22:05:19 +0800

    insert documents into db

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -6,14 +6,45 @@ const databaseName = "task-manager";
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) console.log("Unable to connect to database!")
+
   console.log("Connected correctly!")
 
   const db = client.db(databaseName);
 
-  db.collection("users").insertOne({
-    name: "Louis",
-    age: 35
-  })
+  // db.collection("users").insertOne({
+  //   name: "Louis",
+  //   age: 35
+  // }, (error, result) => {
+  //   if(error) console.log("Unable to insert user")
 
+  //   console.log(result.ops)
+  // })
+
+  // db.collection("users").insertMany([
+  //   {
+  //     name: "Jane",
+  //     age: 28
+
+  //   }, {
+  //     name: "Gunther",
+  //     age: 33
+  //   }
+  // ], (error, result) => {
+  //   if(error) console.log("Unable to insert user")
+  //   console.log(result.ops)
+  // })
+
+    db.collection('tasks').insertMany([
+      {
+        description: "Buy avengers tickets",
+        completed: false
+      }, {
+        description: "Drive to Mioli",
+        completed:  false
+      }
+    ], (error, result) => {
+      if(error) console.log("Unable to insert user")
+      console.log(result.ops);
+    })
 });
 
