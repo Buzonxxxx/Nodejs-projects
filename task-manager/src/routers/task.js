91f1edd4d663631d306a3627ce26da7b0a1d02bf
louis.liao <buzonliao@gmail.com>
2019-05-18 17:49:50 +0800

    add middleware for user and hash password

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Task = require("../models/task")
+const Task = require("../models/task");
 const router = new express.Router();
 
 router.post("/tasks", async (req, res) => {
@@ -42,12 +42,11 @@ router.patch("/tasks/:id", async (req, res) => {
   if (!isValidOperation)
     return res.status(400).send({ error: "Invalid updates!" });
 
-  const _id = req.params.id;
   try {
-    const task = await Task.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true
-    });
+    const task = await Task.findById(req.params.id);
+    updates.forEach(update => (task[update] = req.body[update]));
+    await task.save;
+
     if (!task) return res.status(404).send();
     res.send(task);
   } catch (e) {
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user")
+const User = require("../models/user");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -41,12 +41,12 @@ router.patch("/users/:id", async (req, res) => {
   if (!isValidOperation)
     return res.status(400).send({ error: "Invalid updates!" });
 
-  const _id = req.params.id;
   try {
-    const user = await User.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true
-    });
+    const user = await User.findById(req.params.id);
+    
+    updates.forEach(update => (user[update] = req.body[update]));
+    await user.save();
+
     if (!user) return res.status(404).send();
     res.send(user);
   } catch (e) {
