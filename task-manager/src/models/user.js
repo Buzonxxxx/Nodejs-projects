d4995c2bba2607d41d78592b6e9497afd50e44fe
louis.liao <buzonliao@gmail.com>
2019-05-18 18:32:12 +0800

    add user login api

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -36,6 +37,19 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  try {
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("Unable to login");
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error("Unable to login");
+    return user;
+  } catch (e) {
+    return e.message;
+  }
+};
+
+// Hash plain text password before saving
 userSchema.pre("save", async function(next) {
   const user = this;
   if (user.isModified("password"))
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -12,6 +12,18 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 router.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
@@ -43,7 +55,7 @@ router.patch("/users/:id", async (req, res) => {
 
   try {
     const user = await User.findById(req.params.id);
-    
+
     updates.forEach(update => (user[update] = req.body[update]));
     await user.save();
 
