3e0395e8355490d739870bc6a2fc929ab76c7fad
louis.liao <buzonliao@gmail.com>
2019-07-16 06:46:59 +0800

    relationship between user and task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,3 +14,18 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`server is up on port ${port}`);
 });
+
+
+const Task = require('./models/task')
+const User = require('./models/user')
+const main = async () => {
+  // const task = await Task.findById('5d2cfe57a07ee37d798de453')
+  // await task.populate('owner').execPopulate()
+  // console.log(task.owner)
+  
+  const user = await User.findById('5d2cfe4ea07ee37d798de451')
+  await user.populate('tasks').execPopulate()
+  console.log(user.tasks)
+}
+
+main()
